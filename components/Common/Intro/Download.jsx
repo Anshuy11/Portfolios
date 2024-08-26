9ffa33e8bbaf66d36f708360e59ef64759c19e37
Anshu <anshuy115@gmail.com>
2024-08-26 23:59:07 +0530

    uploded new resume

--- components/Common/Intro/Download.jsx
+++ components/Common/Intro/Download.jsx
@@ -7,7 +7,7 @@ const Download = ({ icon }) => {
         target="_blank"
         className="flex flex-row text-LightGray items-center gap-x-4 pb-14 pt-4"
       >
-        <span className="text-Snow">Download Resume</span>
+        <span className="text-Snow">Resume</span>
         <span>{icon}</span>
       </Link>
     </>
